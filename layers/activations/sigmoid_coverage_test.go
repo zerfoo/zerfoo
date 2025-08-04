ff24ab7ad97b2a6f06f3525499f209b2980b58b2
David Ndungu <ndungu.sink@gmail.com>
2025-08-03 20:56:04 -0700

    fix: Resolve unused-parameter revive issue in layers/activations/sigmoid_coverage_test.go

--- layers/activations/sigmoid_coverage_test.go
+++ layers/activations/sigmoid_coverage_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/zerfoo/zerfoo/testing/testutils"
 )
 
-func TestSigmoid_Error(t *testing.T) {
+func TestSigmoid_Error(_ *testing.T) {
 	engine := compute.NewCPUEngine[float32](numeric.Float32Ops{})
 	ops := numeric.Float32Ops{}
 
