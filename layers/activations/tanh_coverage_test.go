d863b9a21b856822dfc8866fde5d59ca9b245a41
David Ndungu <ndungu.sink@gmail.com>
2025-08-03 20:57:08 -0700

    fix: Resolve unused-parameter revive issue in layers/activations/tanh_coverage_test.go

--- layers/activations/tanh_coverage_test.go
+++ layers/activations/tanh_coverage_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/zerfoo/zerfoo/testing/testutils"
 )
 
-func TestTanh_Error(t *testing.T) {
+func TestTanh_Error(_ *testing.T) {
 	engine := compute.NewCPUEngine[float32](numeric.Float32Ops{})
 	ops := numeric.Float32Ops{}
 
