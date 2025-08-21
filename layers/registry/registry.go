3636db4d7abf955c581c42c7ede33ce877eb4c1b
David Ndungu <ndungu.sink@gmail.com>
2025-08-21 07:15:42 -0700

    refactor: Centralize layer registration to break import cycle

---
+++ layers/registry/registry.go
@@ -0,0 +1,22 @@
+// package registry provides a central registration point for all layer builders.
+// This package exists to break the import cycle between the model package and the
+// individual layer packages.
+package registry
+
+import (
+	"github.com/zerfoo/zerfoo/layers/activations"
+	"github.com/zerfoo/zerfoo/layers/embeddings"
+	"github.com/zerfoo/zerfoo/layers/normalization"
+	"github.com/zerfoo/zerfoo/model"
+	"github.com/zerfoo/zerfoo/tensor"
+)
+
+func init() {
+	// Activations
+	model.RegisterLayer("FastGelu", activations.BuildFastGelu[tensor.Float32])
+
+	// Normalization
+	model.RegisterLayer("RMSNorm", normalization.BuildRMSNorm[tensor.Float32])
+	model.RegisterLayer("SimplifiedLayerNormalization", normalization.BuildSimplifiedLayerNormalization[tensor.Float32])
+	model.RegisterLayer("SkipSimplifiedLayerNormalization", normalization.BuildSkipSimplifiedLayerNormalization[tensor.Float32])
+}
