748417c945fece9d35da45c716f34fc0887ddc13
David Ndungu <ndungu.sink@gmail.com>
2025-08-21 07:15:57 -0700

    refactor: Make layer builders public

--- layers/normalization/registry.go
+++ layers/normalization/registry.go
@@ -4,20 +4,13 @@ package normalization
 import (
 	"fmt"
 
-	"github.com/zerfoo/float16"
 	"github.com/zerfoo/zerfoo/compute"
 	"github.com/zerfoo/zerfoo/graph"
-	"github.com/zerfoo/zerfoo/model"
 	"github.com/zerfoo/zerfoo/numeric"
 	"github.com/zerfoo/zerfoo/tensor"
 )
 
-func init() {
-	model.RegisterLayer("RMSNorm", buildRMSNorm[float16.Float16])
-	// Add registrations for other supported types like float32 if needed.
-}
-
-func buildRMSNorm[T tensor.Numeric](
+func BuildRMSNorm[T tensor.Numeric](
 	engine compute.Engine[T],
 	ops numeric.Arithmetic[T],
 	name string,
@@ -34,10 +27,75 @@ func buildRMSNorm[T tensor.Numeric](
 		return nil, fmt.Errorf("missing required attribute: epsilon")
 	}
 
-	epsilon, ok := epsilonAttr.(float64)
-	if !ok {
-		return nil, fmt.Errorf("attribute 'epsilon' has incorrect type: expected float64, got %T", epsilonAttr)
+	var epsilon float64
+	switch v := epsilonAttr.(type) {
+	case float64:
+		epsilon = v
+	case float32:
+		epsilon = float64(v)
+	default:
+		return nil, fmt.Errorf("attribute 'epsilon' has incorrect type: expected float64 or float32, got %T", epsilonAttr)
 	}
 
 	return NewRMSNormFromParam(engine, ops, ops.FromFloat64(epsilon), gain)
+}
+
+func BuildSimplifiedLayerNormalization[T tensor.Numeric](
+	engine compute.Engine[T],
+	ops numeric.Arithmetic[T],
+	name string,
+	params map[string]*graph.Parameter[T],
+	attributes map[string]interface{},
+) (graph.Node[T], error) {
+	gain, ok := params[name+"_gain"]
+	if !ok {
+		return nil, fmt.Errorf("missing required parameter: %s_gain", name)
+	}
+
+	epsilonAttr, ok := attributes["epsilon"]
+	if !ok {
+		return nil, fmt.Errorf("missing required attribute: epsilon")
+	}
+
+	var epsilon float64
+	switch v := epsilonAttr.(type) {
+	case float64:
+		epsilon = v
+	case float32:
+		epsilon = float64(v)
+	default:
+		return nil, fmt.Errorf("attribute 'epsilon' has incorrect type: expected float64 or float32, got %T", epsilonAttr)
+	}
+
+	return NewSimplifiedLayerNormalization(engine, ops, gain.Value, T(epsilon))
+}
+
+func BuildSkipSimplifiedLayerNormalization[T tensor.Numeric](
+	engine compute.Engine[T],
+	ops numeric.Arithmetic[T],
+	name string,
+	params map[string]*graph.Parameter[T],
+	attributes map[string]interface{},
+) (graph.Node[T], error) {
+	gain, ok := params[name+"_gain"]
+	if !ok {
+		return nil, fmt.Errorf("missing required parameter: %s_gain", name)
+	}
+
+	epsilonAttr, ok := attributes["epsilon"]
+	if !ok {
+		return nil, fmt.Errorf("missing required attribute: epsilon")
+	}
+
+	var epsilon float64
+	switch v := epsilonAttr.(type) {
+	case float64:
+		epsilon = v
+	case float32:
+		epsilon = float64(v)
+	default:
+		return nil, fmt.Errorf("attribute 'epsilon' has incorrect type: expected float64 or float32, got %T", epsilonAttr)
+	}
+
+	return NewSkipSimplifiedLayerNormalization(engine, ops, gain.Value, T(epsilon))
 }
