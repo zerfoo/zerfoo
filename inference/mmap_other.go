7b174d43c37006f0325b9a13d42b826e7bbc3bd9
David Ndungu <ndungu.sink@gmail.com>
2026-03-16 02:41:33 -0700

    ci(release): add Homebrew cask to GoReleaser pipeline
    Add homebrew_casks config so releases auto-publish to zerfoo/homebrew-tap.
    Remove dead ZMF mmap stub that broke Windows cross-compilation.

--- inference/mmap_other.go
+++ inference/mmap_other.go
@@ -1,20 +1,3 @@
 //go:build !unix
 
 package inference
-
-import (
-	"fmt"
-	"io"
-
-	"github.com/zerfoo/ztensor/compute"
-	"github.com/zerfoo/zerfoo/model"
-)
-
-// loadZMFWithMmap is not supported on non-unix platforms.
-func loadZMFWithMmap(
-	_ compute.Engine[float32],
-	_ string,
-	_ []model.BuildOption,
-) (*model.Model[float32], io.Closer, error) {
-	return nil, nil, fmt.Errorf("mmap loading is not supported on this platform")
-}
