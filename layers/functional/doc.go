e5449e85d1bef80feb8a283e9d981f479e6e500c
David Ndungu <ndungu.sink@gmail.com>
2026-04-02 23:32:14 -0700

    feat(layers): add functional Linear and MultiHeadAttention wrappers

---
+++ layers/functional/doc.go
@@ -0,0 +1,3 @@
+// Package functional provides stateless neural network operations that operate
+// directly on tensors via a compute.Engine, without wrapping them in layer structs.
+package functional
